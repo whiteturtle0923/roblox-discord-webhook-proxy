dfd129366dd815d89e8ae6e9e00467f7ccc84a9b
whiteturtle0923 <138399870+whiteturtle0923@users.noreply.github.com>
2024-08-28 19:24:19 -0700

    Delete robloxhttp.lua

--- robloxhttp.lua
+++
@@ -1,4 +0,0 @@
-print("httptest")
-task.wait(0.1)
-local thing = loadstring(game.HttpService:GetAsync("https://raw.githubusercontent.com/whiteturtle0923/roblox-http-test/main/robloxhttp.lua"))
-thing()
